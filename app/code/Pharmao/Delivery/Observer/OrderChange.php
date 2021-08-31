7311d237edd6535d786134ca71526cce3a107983
Naveen Kumar Jain <naveen@pipl.team>
2021-08-31 17:33:38 +0530

    Job validation condition

--- app/code/Pharmao/Delivery/Observer/OrderChange.php
+++ app/code/Pharmao/Delivery/Observer/OrderChange.php
@@ -40,51 +40,50 @@ class OrderChange implements \Magento\Framework\Event\ObserverInterface
         $logger->info('status : ' . $config_status);
         $logger->info('status : ' . $order->getState());
         
-        $data = array (
-          'job' => 
-          array (
-            'assignment_code' => $assignment_code,
-            'transport_type' => 'Bike',
-            'package_type' => 'small',
-            'package_description' => '',
-            'comment' => 'this is a test comment',
-            'is_within_one_hour' => 1,
-            'pickups' => 
-            array (
-              0 => 
-              array (
-                'comment' => 'Rentrez dans la pharmacie, allez au comptoir et demander la commande Pharmao Nom: ' .$order->getCustomerFirstname() . " " . $order->getCustomerLastname(),
-                'contact' => 
-                array (
-                  'firstname' => $this->scopeConfig->getValue('delivery_configuration/global_settings/firstname', $storeScope),
-                  'phone' => $this->scopeConfig->getValue('delivery_configuration/global_settings/phone', $storeScope),
-                ),
-                'address' => $this->scopeConfig->getValue('delivery_configuration/global_settings/address', $storeScope) . ", ". $this->scopeConfig->getValue('delivery_configuration/global_settings/postcode', $storeScope) . " " . $this->scopeConfig->getValue('delivery_configuration/global_settings/city', $storeScope) . ", France",
-              ),
-            ),
-            'dropoffs' => 
-            array (
-              0 => 
-              array (
-                'comment' => $street_1,
-                'address' => $full_address,
-                'contact' => 
-                array (
-                  'firstname' => $order->getCustomerFirstname(),
-                  'phone' => $order->getShippingAddress()->getTelephone(),
-                  'lastname' => $order->getCustomerLastname(),
-                  'email' => $order->getCustomerEmail(),
-                ),
-              ),
-            ),
-          ),
-        );
-        
-        $logger->info('data : ' . print_r($data, true));
-        $data_json = json_encode($data);
-        
-        
         if ($order->getState() == $config_status) {
+            $data = array (
+              'job' => 
+              array (
+                'assignment_code' => $assignment_code,
+                'transport_type' => 'Bike',
+                'package_type' => 'small',
+                'package_description' => '',
+                'comment' => 'this is a test comment',
+                'is_within_one_hour' => 1,
+                'pickups' => 
+                array (
+                  0 => 
+                  array (
+                    'comment' => 'Rentrez dans la pharmacie, allez au comptoir et demander la commande Pharmao Nom: ' .$order->getCustomerFirstname() . " " . $order->getCustomerLastname(),
+                    'contact' => 
+                    array (
+                      'firstname' => $this->scopeConfig->getValue('delivery_configuration/global_settings/firstname', $storeScope),
+                      'phone' => $this->scopeConfig->getValue('delivery_configuration/global_settings/phone', $storeScope),
+                    ),
+                    'address' => $this->scopeConfig->getValue('delivery_configuration/global_settings/address', $storeScope) . ", ". $this->scopeConfig->getValue('delivery_configuration/global_settings/postcode', $storeScope) . " " . $this->scopeConfig->getValue('delivery_configuration/global_settings/city', $storeScope) . ", France",
+                  ),
+                ),
+                'dropoffs' => 
+                array (
+                  0 => 
+                  array (
+                    'comment' => $street_1,
+                    'address' => $full_address,
+                    'contact' => 
+                    array (
+                      'firstname' => $order->getCustomerFirstname(),
+                      'phone' => $order->getShippingAddress()->getTelephone(),
+                      'lastname' => $order->getCustomerLastname(),
+                      'email' => $order->getCustomerEmail(),
+                    ),
+                  ),
+                ),
+              ),
+            );
+            
+            $logger->info('data : ' . print_r($data, true));
+            $data_json = json_encode($data);
+            
             $validate_url = 'https://delivery-sandbox.pharmao.fr/v1/job/validate';
             $headers = ["Content-Type" => "application/json", "Authorization" => $access_token];
             $this->_curl->setHeaders($headers);
