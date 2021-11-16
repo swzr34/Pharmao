e5569e4c9e9bf31175e29700a2f77d76e4a9baf5
Naveen Kumar Jain <naveen@pipl.team>
2021-11-16 04:22:14 -0500

    solve place order error

--- Observer/OrderChangePlaceOrder.php
+++ Observer/OrderChangePlaceOrder.php
@@ -37,63 +37,61 @@ class OrderChangePlaceOrder
         \Magento\Sales\Api\OrderManagementInterface $orderManagementInterface,
         $order
     ) {
-        $orderId = $order->getId();
-        $storeId = $order->getStore()->getId();
-        $this->model->setStoreId($storeId);
-        if (!$this->model->isEnabled()) {
-            return false;
-        }
-        
-        if ($order->getShippingAddress()) {
-            $assignmentCode = $this->helper->generateRandomNumber();
-            $addressData = $this->helper->getFullAddress($order->getShippingAddress());
-            $fullAddress = $addressData['full_address'];
-            $configStatus = $this->model->getConfigData('pharmao_delivery_active_status');
-            $configState = $this->model->getConfigData('pharmao_delivery_active_stat');
-            $configIsWithinOneHour = $this->model->getConfigData('delivery_type');
-            $isPharmaoOrder = false;
-            
-            if ($configIsWithinOneHour == 2) {
-                if ($order->getShippingMethod() == $this->_within_day_code || $order->getShippingMethod() == $this->_within_hour_code) {
-                    $isPharmaoOrder = true;
-                    if ($order->getShippingMethod() == $this->_within_day_code) {
-                        $configIsWithinOneHour = 0;
-                    } else {
-                        $configIsWithinOneHour = 1;
+        if ($this->model->isEnabled()) {
+            $orderId = $order->getId();
+            $storeId = $order->getStore()->getId();
+            $this->model->setStoreId($storeId);
+            if ($order->getShippingAddress()) {
+                $assignmentCode = $this->helper->generateRandomNumber();
+                $addressData = $this->helper->getFullAddress($order->getShippingAddress());
+                $fullAddress = $addressData['full_address'];
+                $configStatus = $this->model->getConfigData('pharmao_delivery_active_status');
+                $configState = $this->model->getConfigData('pharmao_delivery_active_stat');
+                $configIsWithinOneHour = $this->model->getConfigData('delivery_type');
+                $isPharmaoOrder = false;
+                
+                if ($configIsWithinOneHour == 2) {
+                    if ($order->getShippingMethod() == $this->_within_day_code || $order->getShippingMethod() == $this->_within_hour_code) {
+                        $isPharmaoOrder = true;
+                        if ($order->getShippingMethod() == $this->_within_day_code) {
+                            $configIsWithinOneHour = 0;
+                        } else {
+                            $configIsWithinOneHour = 1;
+                        }
                     }
                 }
-            }
-    
-            if ($isPharmaoOrder) {    
-                if ($order->getStatus() == $configStatus && $order->getState() == $configState) {
-    
-                    $pharmaoDeliveryJobInstance = $this->helper->getPharmaoDeliveryJobInstance();
-                    
-                    $response = $pharmaoDeliveryJobInstance->validateAndCreateJob([
-                        'order_amount' => $order->getGrandTotal(),
-                        'assignment_code' => $assignmentCode,
-                        'order_id' => $order->getEntityId(),
-                        'is_within_one_hour' => $configIsWithinOneHour,
-                        'customer_firstname' => $order->getCustomerFirstname(),
-                        'customer_lastname' => $order->getCustomerLastname(),
-                        'customer_comment' => $addressData['street_1'],
-                        'customer_address' => $fullAddress,
-                        'customer_phone' => $order->getShippingAddress()->getTelephone(),
-                        'customer_email' => $order->getCustomerEmail(),
-                    ]);
         
-                    if ($response && isset($response->code) && 200 == $response->code) {
-                        $model = $this->_jobFactory->create();
-                        $model->addData([
-                            "order_id" => $order->getEntityId(),
-                            "job_id" => $response->data->job_id,
-                            "store_id" => $order->getStore()->getStoreId(),
-                            "status" => $response->data->status,
-                            "address" => $fullAddress,
-                            "added" => date("Y-m-d H:i:s")
+                if ($isPharmaoOrder) {    
+                    if ($order->getStatus() == $configStatus && $order->getState() == $configState) {
+        
+                        $pharmaoDeliveryJobInstance = $this->helper->getPharmaoDeliveryJobInstance();
+                        
+                        $response = $pharmaoDeliveryJobInstance->validateAndCreateJob([
+                            'order_amount' => $order->getGrandTotal(),
+                            'assignment_code' => $assignmentCode,
+                            'order_id' => $order->getEntityId(),
+                            'is_within_one_hour' => $configIsWithinOneHour,
+                            'customer_firstname' => $order->getCustomerFirstname(),
+                            'customer_lastname' => $order->getCustomerLastname(),
+                            'customer_comment' => $addressData['street_1'],
+                            'customer_address' => $fullAddress,
+                            'customer_phone' => $order->getShippingAddress()->getTelephone(),
+                            'customer_email' => $order->getCustomerEmail(),
                         ]);
-        
-                        $saveData = $model->save();
+            
+                        if ($response && isset($response->code) && 200 == $response->code) {
+                            $model = $this->_jobFactory->create();
+                            $model->addData([
+                                "order_id" => $order->getEntityId(),
+                                "job_id" => $response->data->job_id,
+                                "store_id" => $order->getStore()->getStoreId(),
+                                "status" => $response->data->status,
+                                "address" => $fullAddress,
+                                "added" => date("Y-m-d H:i:s")
+                            ]);
+            
+                            $saveData = $model->save();
+                        }
                     }
                 }
             }
